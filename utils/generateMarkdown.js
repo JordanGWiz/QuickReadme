855b1fedd025230cf7ae4e158babf6655d3221c6
Jordan <jordan.gonzales17@hotmail.com>
2024-06-03 21:16:48 -0700

    Finished the generateMarkdown for the renderLicense sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,25 @@
 function renderLicenseBadge(license) {
-  if (!license || license === "No license") {
-    return "![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)";
+  if (license !== "No license") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
   return "";
 }
 
 function renderLicenseLink(license) {
-  if (!license || license === "No license") {
-    return "\n* [License](#license)\n";
+  if (license !== "No license") {
+    return `\n* [License](#license)\n`;
   }
   return "";
 }
 
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "No license") {
+    return `## License 
+    
+    This project is licensed under the ${license} license`;
+  }
+  return "";
+}
 
 function generateMarkdown(data) {
   return `# ${data.projectName}
@@ -47,8 +54,7 @@ ${data.contribution}
 ## Tests
 ${data.test}
 
-## License
-This project is licensed under the ${data.license}.
+${renderLicenseSection(data.license)}
 
 ## Questions
 If you have any questions, don't hesitate to reach out to me at [${
