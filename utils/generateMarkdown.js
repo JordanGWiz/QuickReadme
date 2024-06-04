a205b028c73b10a957bcbdb162d266385268a134
Jordan <jordan.gonzales17@hotmail.com>
2024-06-03 20:53:57 -0700

    Created code for the first function of generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,14 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === "No license") {
+    return "![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)";
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-//Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectName}
 ${renderLicenseBadge(data.license)}
